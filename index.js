e28ac16d1736835946daba9cdd48e45d42ed0541
Levon Kirakosyan <noosx.e@gmail.com>
2013-11-19 13:36:15 +0400

    ordered add, remove, get, range, length methods working

--- index.js
+++ index.js
@@ -5,22 +5,48 @@ module.exports = (function() {
 
   var Cache = function(options) {
     this.values = [];
+    this.options = options;
   };
 
-  Cache.prototype.add = function() {
+  Cache.prototype.add = function(item) {
+    if (this.values.length === 0 || !this.options.callback) {
+      this.values.push(item);
+      return;
+    }
 
+    var added = false;
+
+    _.forEach(this.values, function(val, key) {
+      if (this.options.callback(val, item)) {
+        this.values.splice(key, 0, item);
+        added = true;
+        return false;
+      }
+    }, this);
+
+    if (!added) {
+      this.values.push(item);
+    }
   };
 
-  Cache.prototype.remove = function() {
-
+  Cache.prototype.remove = function(target) {
+    if (isNaN(target)) {
+      this.values = _.without(this.values, target);
+    } else {
+      this.values.splice(target, 1);
+    }
   };
 
   Cache.prototype.get = function(index) {
-
+    return index < this.values.length ? this.values[index] : null;
   };
 
   Cache.prototype.range = function(begin, count) {
+    return this.values.slice(begin, begin + count);
+  };
 
+  Cache.prototype.length = function() {
+    return this.values.length;
   };
 
   return Cache;
--- test/index.js
+++ test/index.js
@@ -2,6 +2,16 @@
   "use strict";
 
   var Cache = require('../index')
-    , cache = new Cache();
+    , cache = new Cache({
+      callback: function(ob1, ob2) {
+        return ob1 > ob2;
+      }
+    });
+
+  for(var i = 0; i < 10; ++i) {
+    cache.add(Math.floor(Math.random() * 100 ));
+  }
+
+  console.log(cache.values);
 
 }());
