1c5d7c27fa62e69b4a1b3ed439ee414bd01de4ae
Levon Kirakosyan <noosx.e@gmail.com>
2013-11-19 12:55:27 +0400

    package.json added, cache class skeleton added

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2013 Levon
+Copyright (c) 2013 Levon Kirakosyan <<noosx.e@gmail.com>>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
---
+++ index.js
@@ -0,0 +1,28 @@
+var _ = require('lodash');
+
+module.exports = (function() {
+  "use strict";
+
+  var Cache = function(options) {
+    this.values = [];
+  };
+
+  Cache.prototype.add = function() {
+
+  };
+
+  Cache.prototype.remove = function() {
+
+  };
+
+  Cache.prototype.get = function(index) {
+
+  };
+
+  Cache.prototype.range = function(begin, count) {
+
+  };
+
+  return Cache;
+
+}());
